1854ad4ca19da7d4764423727fe2deccd62e25ab
Mike Gardner <mjgardner@abitofhelp.com>
2018-01-06 23:08:54 -0700

    Added GO's dependency manager and testify/assert to the test file.

--- clean/domain/entities/tests/Motorcycle_test.go
+++ clean/domain/entities/tests/Motorcycle_test.go
@@ -1,11 +1,14 @@
 package tests
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGenerateQRCodeReturnsValue(t *testing.T) {
 	result := 123
+
+	assert.Equal(t, 123, 123, "they should be equal")
 
 	if result != 123 {
 		t.Errorf("Invalid result value.")
